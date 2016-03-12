bb276276157594a13539c86fd556da50fc799b15
Rebecca Reed <rebeccareed212@hotmail.com>
2016-03-12 17:35:48 +0000

    Setting up the page templates

---
+++ simonmuris/page-templates/page_process.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Process */ ?>
+
+<?get_header(); ?>
+
+	<div class="row">
+		<div class="col-sm-12 process">
+			<div class="process__information">
+			</div>
+
+			<div class="process__background">
+				<img src="/simonmuris-html/images/hendecagon.png" alt="hendecagon">
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
