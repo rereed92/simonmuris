448e37fd22ec89dd200522e6e24783c81910158a
Rebecca Reed <rebeccareed212@hotmail.com>
2016-03-21 22:01:22 +0000

    Adding in the repeater field for the process page

--- simonmuris/page-templates/page_process.php
+++ simonmuris/page-templates/page_process.php
@@ -5,6 +5,27 @@
 	<div class="row">
 		<div class="col-lg-12 process">
 			<div class="process__information">
+
+				<?php if( have_rows('process_content') ): ?>
+
+					<?php while( have_rows('process_content') ): the_row(); 
+						$image = get_sub_field('process_image');
+						$content = get_sub_field('process_text');
+
+					?>
+						<div class="process__container">
+							<div class="process__image">
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+							</div>
+
+							<div class="process__text">
+								 <?php echo $content; ?>
+							</div>
+						</div>
+
+					<?php endwhile; ?>
+
+				<?php endif; ?>
 			</div>
 
 			<div class="process__background">
