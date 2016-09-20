f75c5f2f5b0d262d126a3809d6b97dcf1825d3cb
Rebecca Reed <rebeccareed212@hotmail.com>
2016-09-21 00:00:30 +0100

    Update the gallery template

--- simonmuris/page-templates/page_gallery.php
+++ simonmuris/page-templates/page_gallery.php
@@ -9,21 +9,24 @@
 			$images = get_field('gallery');
 
 			if( $images ): ?>
+				<div class="gallery">
 
-			    <ul class="gallery">
+				    <ul class="gallery__items">
 
-			        <?php foreach( $images as $image ): ?>
+				        <?php foreach( $images as $image ): ?>
 
-			            <li class="gallery__item col-sm-6 col-md-4 col-lg-2">
-			                <a href="<?php echo $image['url']; ?>" class="gallery__link">
-			                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" class="gallery__image">
-			                </a>
-			                <!-- <p><?php echo $image['caption']; ?></p> -->
-			            </li>
+				            <li class="gallery__item col-sm-6 col-md-4 col-lg-2">
+				                <a href="<?php echo $image['url']; ?>" class="gallery__link">
+				                     <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" class="gallery__image">
+				                </a>
+				                <!-- <p><?php echo $image['caption']; ?></p> -->
+				            </li>
 
-			        <?php endforeach; ?>
+				        <?php endforeach; ?>
 
-			    </ul>
+				    </ul>
+
+			    </div>
 
 			<?php endif; ?>
 
